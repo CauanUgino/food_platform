0f3fc5088d829a4744d1919c1d99bafed0dff6ba
Cauan179 <cauanuginofilho@gmail.com>
2026-07-15 14:00:12 -0300

    Versão final do app

--- food_platform/food_platform/settings.py
+++ food_platform/food_platform/settings.py
@@ -75,13 +75,16 @@ TEMPLATES = [
 WSGI_APPLICATION = 'food_platform.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
+
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'comai_db',
+        'USER': 'root',
+        'PASSWORD': 'cAuan1319@',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
